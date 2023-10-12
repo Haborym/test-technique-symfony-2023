57a705c5793270ce5597524a2f63192fc66bfd8c
Arthur Vadrot <krafteck666@gmail.com>
2023-10-12 17:15:56 +0200

    feat: added the signin form and logic
    The user can now create an account, connect to an account and disconnect

--- symphony-project/src/Controller/HomeController.php
+++ symphony-project/src/Controller/HomeController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class HomeController extends AbstractController
 {
+    private $user;
+
     #[Route('/', name: 'app_home')]
     public function homePage(): Response
     {
         return $this->render('home/index.html.twig', [
-            
+
         ]);
     }
 
--- symphony-project/src/Controller/LoginController.php
+++ symphony-project/src/Controller/LoginController.php
@@ -9,27 +9,13 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/sign-up', name: 'app_login')]
+    #[Route('/sign-in', name: 'app_login')]
     public function singUp(AuthenticationUtils $authenticationUtils): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
 
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('connexion/signUp.html.twig', [
-            'controller_name' => 'LoginController',
-            'last_username' => $lastUsername,
-            'error'         => $error,
-        ]);
-    }
-
-    #[Route('/sign-in', name: 'app_create_login')]
-    public function signIn(AuthenticationUtils $authenticationUtils): Response
-    {
-        $error = $authenticationUtils->getLastAuthenticationError();
-
-        $lastUsername = $authenticationUtils->getLastUsername();
-        
         return $this->render('connexion/signIn.html.twig', [
             'controller_name' => 'LoginController',
             'last_username' => $lastUsername,
